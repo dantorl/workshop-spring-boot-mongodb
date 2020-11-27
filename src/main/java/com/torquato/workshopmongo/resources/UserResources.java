981507c43cbddcaebfad53e76daf426e452c0495
Daniel Torquato <daniel.torquato@hotmail.com>
2020-11-26 21:56:05 -0300

    Criar tratamento de exception

--- src/main/java/com/torquato/workshopmongo/resources/UserResources.java
+++ src/main/java/com/torquato/workshopmongo/resources/UserResources.java
@@ -1,16 +1,20 @@
 package com.torquato.workshopmongo.resources;
 
 import com.torquato.workshopmongo.domain.User;
+import com.torquato.workshopmongo.dto.UserDTO;
 import com.torquato.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -20,10 +24,19 @@ public class UserResources {
     private UserService userService;
 
     @GetMapping("/lista")
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
 
         List<User> list = userService.findAll();
+        List<UserDTO> listDto = list.stream().map( x -> new UserDTO(x)).collect(Collectors.toList());
 
-        return ResponseEntity.ok(list);
+        return ResponseEntity.ok(listDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+
+        Optional<User> user = userService.findById(id);
+
+        return ResponseEntity.ok(new UserDTO(user.get()));
     }
 }
