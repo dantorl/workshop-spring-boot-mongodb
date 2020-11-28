12900c4d4a31f2425af62ff4363647f86bf788ec
Daniel Torquato <daniel.torquato@hotmail.com>
2020-11-27 22:14:47 -0300

    Incluir AuthorDTO

--- src/main/java/com/torquato/workshopmongo/config/Instantiation.java
+++ src/main/java/com/torquato/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.torquato.workshopmongo.config;
 
 import com.torquato.workshopmongo.domain.Post;
 import com.torquato.workshopmongo.domain.User;
+import com.torquato.workshopmongo.dto.AuthorDTO;
 import com.torquato.workshopmongo.repository.PostRepository;
 import com.torquato.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,14 +30,16 @@ public class Instantiation implements CommandLineRunner {
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "teste", "teste",maria);
-        Post post2 = new Post(null, sdf.parse("21/04/2018"), "teste 2", "teste body",maria);
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "teste", "teste",new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("21/04/2018"), "teste 2", "teste body",new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1,post2));
 
     }
